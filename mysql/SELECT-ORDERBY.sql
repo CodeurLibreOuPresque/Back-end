13a67b26134d67fe2bd3a5d13f9c20d95646a287
CodeurLibreOuPresque <mathsperformances@gmail.com>
2018-05-29 21:38:35 +0200

    Create SELECT-ORDERBY.sql

---
+++ mysql/SELECT-ORDERBY.sql
@@ -0,0 +1,8 @@
+// Ordonne resultat d'un requete dans l'ordre désiré
+
+SELECT column1, column2, ...
+FROM table_name
+ORDER BY column1, column2, ... ASC|DESC;
+
+SELECT * FROM Customers
+ORDER BY Country ASC, CustomerName DESC;
