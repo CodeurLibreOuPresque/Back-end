a0a51460fca6524cc9a52db85049f3a4ec5dff07
CodeurLibreOuPresque <mathsperformances@gmail.com>
2018-05-29 21:29:22 +0200

    Create SELECT-WHERE.sql

---
+++ mysql/SELECT-WHERE.sql
@@ -0,0 +1,12 @@
+// Selectionne tel(s) colonne(s) d'une table à la conditon ...
+// WHERE permet d'inserer une condition
+
+SELECT column1, column2, ...
+FROM table_name
+WHERE condition;
+
+SELECT * FROM Customers
+WHERE Country='Mexico';
+
+SELECT * FROM Customers
+WHERE CustomerID=1;
