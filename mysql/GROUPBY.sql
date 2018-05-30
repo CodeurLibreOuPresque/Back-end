08973655d2f4f4387819cd48279a07409489243e
CodeurLibreOuPresque <mathsperformances@gmail.com>
2018-05-30 15:52:27 +0200

    Create GROUPBY.sql

---
+++ mysql/GROUPBY.sql
@@ -0,0 +1,8 @@
+// The GROUP BY statement is often used with aggregate functions (COUNT, MAX, MIN, SUM, AVG) 
+// to group the result-set by one or more columns.
+
+SELECT column_name(s)
+FROM table_name
+WHERE condition
+GROUP BY column_name(s)
+ORDER BY column_name(s);
