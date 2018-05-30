aba08d3d317eeeef7f25ccd6d4d1800794eab1f5
CodeurLibreOuPresque <mathsperformances@gmail.com>
2018-05-30 14:29:06 +0200

    Create SELECT-COUNT-SUM-AVG.sql

---
+++ mysql/SELECT-COUNT-SUM-AVG.sql
@@ -0,0 +1,15 @@
+// COUNT() retourne le nombre de ligne repondant à un critére
+// AVG() retourne la moyenne des colonnes numerique selectionnées
+// SUM() retourne le total des colonnes numeriques selectionnées
+
+SELECT COUNT(column_name)
+FROM table_name
+WHERE condition;
+
+SELECT AVG(column_name)
+FROM table_name
+WHERE condition;
+
+SELECT SUM(column_name)
+FROM table_name
+WHERE condition;
