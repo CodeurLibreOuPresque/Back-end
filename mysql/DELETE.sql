fb0a5f3bf8a46ce2264ca2caad79e750402b74df
CodeurLibreOuPresque <mathsperformances@gmail.com>
2018-05-30 13:28:15 +0200

    Create DELETE.sql

---
+++ mysql/DELETE.sql
@@ -0,0 +1,11 @@
+// Supprimer ligne d'une table en fonction condition ou pas (totalité)
+
+DELETE FROM table_name
+WHERE condition;
+
+DELETE FROM Customers
+WHERE CustomerName='Alfreds Futterkiste';
+
+DELETE FROM table_name;
+
+DELETE * FROM table_name
