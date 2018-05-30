3fe2c5021fd89e68921990be468c960bf5ba6973
CodeurLibreOuPresque <mathsperformances@gmail.com>
2018-05-30 14:36:32 +0200

    Update SELECT-WHERE-LIKE.sql

--- mysql/SELECT-WHERE-LIKE.sql
+++ mysql/SELECT-WHERE-LIKE.sql
@@ -1,6 +1,7 @@
 // Permet de mettre une condition spécifique à un critére de recherche textuel
 // % représente 0, 1 ou une plus caratére
-// _ représente 1 seul caractére
+// _ représente 1 seul caractére 
+// REGEX
 
 SELECT column1, column2, ...
 FROM table_name
